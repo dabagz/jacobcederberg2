8ea7217c440aa76f55e999cb9fee391204ef7beb
Jacob Cederberg <jacobcederberg@huskers.unl.edu>
2018-01-25 00:43:46 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,24 @@
+/*
+**csce155e 5b
+**January 23 1018
+** hello.c                                                                     +
+** jacobcederberg@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main
+(
+  int argc,
+  char *argv[]
+)
+
+{
+
+  fprintf(stdout, "%s\n", "Jacob Cederberg");
+  
+  return EXIT_SUCCESS;
+}
+
+/* -- EOF -- */
